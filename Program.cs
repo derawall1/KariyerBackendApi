cafd2d0ad74a8c3a2e0324a4dcdefdb137a01504
derawall1 <derawall@gmail.com>
2022-06-07 10:29:24 +0500

    pushed database changes

--- Program.cs
+++ Program.cs
@@ -12,32 +12,32 @@ AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 // GETTING CONNECTION STRING
 var defaultConnectionString = string.Empty;
 
-//if (builder.Environment.EnvironmentName == "Development")
-//{
-    defaultConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-    
-//}
-//else
-//{
-    // Use connection string provided at runtime by Heroku.
-//    var connectionUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-
-//    connectionUrl = connectionUrl?.Replace("postgres://", string.Empty);
-//    var userPassSide = connectionUrl?.Split("@")[0];
-//    var hostSide = connectionUrl?.Split("@")[1];
-
-//    var user = userPassSide?.Split(":")[0];
-//    var password = userPassSide?.Split(":")[1];
-//    var host = hostSide?.Split("/")[0];
-//    var database = hostSide?.Split("/")[1].Split("?")[0];
-
-//    defaultConnectionString = $"Host={host};Database={database};Username={user};Password={password};SSL Mode=Require;Trust Server Certificate=true";
-////}
-
-
-// Add services to the container.
+if (builder.Environment.EnvironmentName == "Development")
 {
-    builder.Services.AddAutoMapper(typeof(AutoMapperProfile).Assembly);
+    defaultConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+}
+else
+{
+    Use connection string provided at runtime by Heroku.
+    var connectionUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+    connectionUrl = connectionUrl?.Replace("postgres://", string.Empty);
+    var userPassSide = connectionUrl?.Split("@")[0];
+    var hostSide = connectionUrl?.Split("@")[1];
+
+    var user = userPassSide?.Split(":")[0];
+    var password = userPassSide?.Split(":")[1];
+    var host = hostSide?.Split("/")[0];
+    var database = hostSide?.Split("/")[1].Split("?")[0];
+
+    defaultConnectionString = $"Host={host};Database={database};Username={user};Password={password};SSL Mode=Require;Trust Server Certificate=true";
+    }
+
+
+    // Add services to the container.
+    {
+        builder.Services.AddAutoMapper(typeof(AutoMapperProfile).Assembly);
     builder.Services.AddDbContext<DataContext>(option =>
           option.UseNpgsql(defaultConnectionString)
        );
